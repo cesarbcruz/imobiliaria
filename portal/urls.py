628e1585fb7d6545fe467226cd44505a9c0d064b
Cesar Cruz <cesar.analista@gmail.com>
2015-12-20 19:07:56 -0200

    front-end

--- portal/urls.py
+++ portal/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^cadastre_seu_imovel/$', views.cadastre_seu_imovel),
+    url(r'^documentos/$', views.documentos),
+    url(r'^simule_seu_financiamento/$', views.simule_seu_financiamento),
+    url(r'^contatos/$', views.contatos),
 ]
--- portal/views.py
+++ portal/views.py
@@ -8,3 +8,13 @@ def home(request):
 
 def cadastre_seu_imovel(request):
     return render(request, 'portal/cadastre-seu-imovel.html', {})
+
+def documentos(request):
+    return render(request, 'portal/documentos.html', {})
+
+def simule_seu_financiamento(request):
+    return render(request, 'portal/simule-seu-financiamento.html', {})
+
+def contatos(request):
+    return render(request, 'portal/contatos.html', {})
+
