c443a366d38b50ca6e79e3e8824696c16daf8a07
Cesar Cruz <cesar.analista@gmail.com>
2016-01-05 22:48:44 -0200

    pre-implantacao

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import os,sys
-virtenv = os.path.expanduser('~') + '/envcorreta/'
+virtenv = os.path.expanduser('~') + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     if sys.version.split(' ')[0].split('.')[0] == '3':
