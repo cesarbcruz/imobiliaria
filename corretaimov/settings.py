17d2bb357259525a708f73ec61d4cbfa9313e8fd
Cesar Cruz <cesar.analista@gmail.com>
2016-01-06 00:09:00 -0200

    pre-implantacao

--- corretaimov/settings.py
+++ corretaimov/settings.py
@@ -118,16 +118,10 @@ STATICFILES_FINDERS = [
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_local')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static_local'),
+    os.path.join(BASE_DIR, 'static'),
 )
-
-#MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
-#STATIC_ROOT = '../staticfiles'
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
 
 
 
